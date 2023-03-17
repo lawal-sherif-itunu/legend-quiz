478afe6f9f9afac9717edc8fbdea051baf3da34f
lawal-sherif-itunu <63552329+lawal-sherif-itunu@users.noreply.github.com>
2023-03-17 08:01:48 +0100

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,33 @@
+const correctAnswers = ['D', 'D', 'C', 'B', 'B', 'A', 'C', 'B', 'D', 'D'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value, form.q7.value, form.q8.value, form.q9.value, form.q10.value];
+
+    // CHECK ANSWERS
+
+    userAnswers.forEach((answer, index) => {
+        if(answer === correctAnswers[index]){
+            score += 10;
+        }
+    });
+
+    // SHOW RESULT ON PAGE
+    scrollTo(0,0);
+
+    result.style.display ="block";
+
+    let output = 0;
+    const timer = setInterval(() => {
+    result.querySelector('span').textContent = `${output}%`;
+        if (output === score){
+            clearInterval(timer);
+        } else {
+            output++;
+        }
+    }, 20);
+});
